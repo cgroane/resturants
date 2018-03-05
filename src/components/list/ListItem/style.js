0dffe3b47713be6bfbe303f2797fcf194d3e5db9
Connor Roane <connorroane@Connors-MacBook-Pro.local>
2018-03-05 16:36:22 -0600

    change json file

--- src/components/list/ListItem/style.js
+++ src/components/list/ListItem/style.js
@@ -1,3 +1,5 @@
+// import 
+
 export function styleItem(restaurant) {
     return {
             backgroundImage: `linear-gradient(to top, rgb(49,49,49) 0px, transparent, rgba(255,255,255, 0.3) 180px, transparent), url(${restaurant.backgroundImageURL})`,
