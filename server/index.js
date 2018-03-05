afa8c910ba548241d89891168764ebeffdfdbad6
Connor Roane <connorroane@Connors-MacBook-Pro.local>
2018-03-04 21:05:02 -0600

    set up build

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 const port = 3001;
+app.use(express.static(`${__dirname}/../build`));
 
 const restCtrl = require('./controllers/restaurantController');
 
@@ -13,4 +14,10 @@ app.get('/api/restaurants', restCtrl.getRestaurants);
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
+})
+
+const path = require('path')
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 })
