59b8f26de5bf93c84e3df59bf57830bb4a4086a0
Connor Roane <connorroane@Connors-MacBook-Pro.local>
2018-03-06 12:02:36 -0600

    revert to original

--- src/components/list/ListItem/style.js
+++ src/components/list/ListItem/style.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {css} from 'emotion';
 
 export function styleItem(restaurant) {
-    return `background-image: backgroundImage: linear-gradient(to top, rgb(49,49,49) 0px, rgba(255,255,255,0), rgba(255,255,255, 0.3) 180px, rgba(255,255,255,0)), url("${restaurant.backgroundImageURL}");`
+    let img = restaurant.backgroundImageURL;
+    return `background-image: backgroundImage: linear-gradient(to top, rgb(49,49,49) 0px, rgba(255,255,255,0), rgba(255,255,255, 0.3) 180px, rgba(255,255,255,0)), url("${img}");`
 }
