4e10c1e52956da774d6b6376e679668b1b606457
Connor Roane <connorroane@Connors-MacBook-Pro.local>
2018-03-06 13:47:48 -0600

    Add ser ice worker back

--- src/components/list/ListItem/style.js
+++ src/components/list/ListItem/style.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function styleItem(restaurant) {
     let img = restaurant.backgroundImageURL;
     return {
-        backgroundImage: `linear-gradient(to top, rgb(49,49,49) 0px, rgba(255,255,255,0), rgba(255,255,255, 0.3) 180px, rgba(255,255,255,0)), url(${img})`,
+        backgroundImage: 'linear-gradient(to top, rgb(49,49,49) 0px, rgba(255,255,255,0), rgba(255,255,255, 0.3) 180px, rgba(255,255,255,0)), url(' + img + ')',
         backgroundSize: 'cover'
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
 import store from './store';
 
 ReactDOM.render(
+    <Provider store={store}>
     <BrowserRouter>
     {/* provider */}
-        <Provider store={store}>
+        
             <App />
-        </Provider>
-    </BrowserRouter>, 
+        
+    </BrowserRouter>
+    </Provider>, 
 document.getElementById('root'));
+registerServiceWorker();
 
