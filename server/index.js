e49f60f89f1ac21086963670b38f3fac2a7985fe
Connor Roane <connorroane@Connors-MacBook-Pro.local>
2018-03-05 18:10:40 -0600

    fixed port

--- server/index.js
+++ server/index.js
@@ -22,5 +22,5 @@ app.listen(port, () => {
 const path = require('path')
 
 app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, '/../build/index.html'));
 })
